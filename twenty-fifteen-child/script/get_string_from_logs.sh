d634874c11074887207c44a71b95edac7a51bec7
matapatos <andre_gil22@hotmail.com>
2015-07-13 18:59:05 +0100

    ficheiro corrigido get_string_from_logs.sh

--- twenty-fifteen-child/script/get_string_from_logs.sh
+++ twenty-fifteen-child/script/get_string_from_logs.sh
@@ -5,13 +5,15 @@ VARLOGS="auth.log boot btmp daemon.log debug dmesg dpkg.log dpkg.log.1 kern.log 
 cd /var/log
 
 for ii in $VARLOGS; do
-	STRING = $(cat $ii)
-	if [ - n "$STRING" ]; then
+	STRING=$(cat $ii)
+	if [ -n "$STRING" ]; then
         echo $ii"-->" "$STRING" >> /var/log/my_log.log
+	fi
 done
 
 for ii in /var/log/proftpd/controls.log /var/log/proftpd/proftpd.log /var/log/postgresql/postgresql-8.3-main.log /var/log/postgresql/postgresql-8.3-main.log.1 /var/log/apache2/access.log /var/log/apache2/access.log.1 /var/log/apache2/error.log /var/log/apache2/error.log.1; do
-	STRING = $(cat $ii)
-	if [ - n "$STRING" ]; then
+	STRING=$(cat $ii)
+	if [ -n "$STRING" ]; then
         echo $ii"-->" "$STRING" >> /var/log/my_log.log
+	fi
 done
